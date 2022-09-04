de268ba65aff62f36f0385cccdd17ded6023faf7
volad <volad@example.com>
2022-09-04 13:49:29 +0300

    Submit validation

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const sendButton = document.getElementsByClassName("button-container")[0];
+const iconError = document.getElementsByClassName("icon-error")[0];
+const inputContainer = document.getElementsByClassName("input-container")[0];
+const messageError = document.getElementsByClassName("message-error")[0];
+const inputMessage = document.getElementsByClassName("input-message")[0];
+
+const expEmail = /\S+@\S+\.\S{2,3}/;
+
+sendButton.addEventListener("click", () => {
+  const isValid = expEmail.test(inputMessage.value);
+  if (!isValid) {
+    iconError.style.display = "block";
+    messageError.style.display = "block";
+    inputContainer.classList.add("border-error");
+  } else {
+    iconError.style.display = "none";
+    messageError.style.display = "none";
+    inputContainer.classList.remove("border-error");
+  }
+});
